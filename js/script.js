aefde4ae82296bdd8e4a6cf4e792cf2528490e6a
TTonia1 <161315439+TTonia1@users.noreply.github.com>
2024-04-03 09:38:51 -0300

    Firts

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function funcaoMudarTamanho(){
+    document.getElementById("teste").style.fontSize = "35px";
+}
+function funcaoMudarBack(){
+    document.getElementById('teste').style.color="white";
+    document.getElementById('teste').style.backgroundColor="pink";
+}
+function conta(){
+    document.getElementById("teste").innerHTML= 5 + 8;
+}
+//para colocar e lincar as páginas é necessario utilizar <script src="script.js"></script> dps do body//>
